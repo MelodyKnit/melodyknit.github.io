9194cb3150ee5b9673e0b66a53f3e9ea5d1231fa
Melody言夕 <50488999+Melodyknit@users.noreply.github.com>
2020-06-27 11:14:23 +0000

    Add files via upload

---
+++ anime-search/script/data.js
@@ -0,0 +1,16 @@
+const webSite = [{
+    webSite: '末日动漫',
+    search: '/search.php?keyword=',
+    url: 'https://share.acgnx.se',
+    logo: 'https://share.acgnx.se/images/favicon.ico'
+}, {
+    webSite: '动漫花园',
+    search: '/search/',
+    url: 'https://www.dongmanhuayuan.com',
+    logo: 'https://www.dongmanhuayuan.com/static/www/images/logo.png'
+}, {
+    webSite: '东京图书馆',
+    search: '/search.php?terms=',
+    url: 'https://www.tokyotosho.info',
+    logo: 'https://ttcdn.info/favicon.png'
+}];
---
+++ anime-search/script/main.js
@@ -0,0 +1,49 @@
+$.el = tag => $(document.createElement(tag));
+var vue = new Vue({
+    el: '#view',
+    data: {
+        self: {
+            logo: 'http://q1.qlogo.cn/g?b=qq&nk=2711402357&s=640'
+        },
+        webSite: webSite,
+        main: webSite[0],
+        error: false,
+        value: null,
+        tag: null,
+    },
+    methods: {
+        bg(url, color) {
+            return {
+                background: `url(${url}) center center / 80% no-repeat`,
+                backgroundColor: color || 'white'
+            }
+        },
+        el(e, name) {
+            for (let i in e)
+                if (e[i].className == name) return e[i];
+        },
+        sw(e, i) {
+            if (this.tag) this.tag.className = 'list';
+            e = this.el(e.path, 'webSite');
+            e = this.el(e.children, 'list');
+            e.className = 'listTo';
+            this.tag = e;
+            this.main = i;
+        },
+        search(e) {
+            e = this.value;
+            if (e) {
+                e = `${this.main.url}${this.main.search}${e}`;
+                window.open(e);
+            } else this.error = true;
+        },
+        bgcolor() {
+            let attr = e => $('body').attr(e);
+            let color = attr('class') == 'black' ? 'white' : 'black';
+            attr({ class: color });
+            $(this.$refs.bg).css(this.bg(`image/${color}.png`, color));
+        }
+    }
+});
+vue.bgcolor();
+if (new Date().getHours() > 19) vue.bgcolor();
