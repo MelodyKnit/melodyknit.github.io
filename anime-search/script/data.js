acbc9cd71e51861dba9c97acb815295760e4fbf2
MelodyCodes <50488999+Melodyknit@users.noreply.github.com>
2021-03-14 16:33:20 +0800

    Update data.js

--- anime-search/script/data.js
+++ anime-search/script/data.js
@@ -4,6 +4,11 @@ const webSite = [{
     url: 'https://www.dongmanhuayuan.com',
     logo: 'https://www.dongmanhuayuan.com/static/www/images/logo.png'
 }, {
+    webSite: 'ACG动漫',
+    search: '/search?query=',
+    url: 'https://www.agefans.net',
+    logo: 'https://www.agefans.net/favicon.ico'
+}, {
     webSite: '东京图书馆',
     search: '/search.php?terms=',
     url: 'https://www.tokyotosho.info',
